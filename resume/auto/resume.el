ce05bd748b83bbb0ab8104463f49b3ca3c90d29c
Yoshiki Takashima <ytakashima@andrew.cmu.edu>
2023-10-01 20:29:16 -0400

    Clean up resume.

---
+++ resume/auto/resume.el
@@ -0,0 +1,25 @@
+(TeX-add-style-hook
+ "resume"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "11pt" "letterpaper")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("parskip" "parfill")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art11"
+    "parskip"
+    "array"
+    "ifthen")
+   (TeX-add-symbols
+    "namesize"
+    "addressskip"
+    "sectionlineskip"
+    "nameskip"
+    "sectionskip")
+   (LaTeX-add-environments
+    '("rSubsection" 4)
+    '("rSection" 1)))
+ :latex)
+
